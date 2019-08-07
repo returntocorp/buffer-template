dfb8befdf59b18caf443d16b45b011b39700a2d8
Arash Delijani <arashdelijani@gmail.com>
2019-08-06 18:40:28 -0700

    fix analyzer.json analyzer name and make rule code structure similar to that of stadt-explorer

--- src/src/rules/buffer.ts
+++ src/src/rules/buffer.ts
@@ -3,31 +3,30 @@ import * as estree from "estree";
 import { Context, Rule, getType, possibleTypes } from "./index";
 import { NominativeType, TypeKind } from "stadt";
 
+function isBuffer(ty: any) {
+  return ty.type === "Identifier" && ty.name === "Buffer";
+}
+
 // Checks for unsafe usage of the Buffer API, i.e. `new Buffer()`,
 export const rule: Rule = {
-  create(context: Context) {
-    function checkConstructor(node: estree.NewExpression) {
-      const checkId = "buffer_constructor_number"
-      
-      if (!(node.callee.type == "Identifier" && node.callee.name == "Buffer")) {
-        return;
-      }
-
-      /*
-        YOUR CODE GOES HERE!
-        Make sure to only report when Buffer is called with a non-literal number argument!
-      */
- 
-      context.report({
-        node: node,
-        checkId: checkId
-      });
-    }
-
+    create(context: Context) {
     return {
       NewExpression(node: estree.NewExpression) {
-        checkConstructor(node);
+        const checkId = "buffer_constructor_number";
+      
+        if (!(isBuffer(node.callee))) {
+          return;
+        }
+         /*
+          YOUR CODE GOES HERE!
+          Make sure to only report when Buffer is (possibly) called with a non-literal number argument!
+          Literal numbers are almost always ok in real code, because they're usually filled in a loop immediately after instantiation.
+        */
+         context.report({
+          node: node,
+          checkId: checkId
+        });
       }
     };
   }
-};
+}
